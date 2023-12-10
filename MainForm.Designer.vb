8f14cfaeaf18cd638ff6f818360043cffe6b131f
Gilbert Gray <153439215+summit-hen@users.noreply.github.com>
2023-12-10 20:58:47 +0700

    Create MainForm.Designer.vb

---
+++ MainForm.Designer.vb
@@ -0,0 +1,11 @@
+' MainForm.Designer.vb
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    ' ... Auto-generated form design code ...
+
+    Private Sub InitializeComponent()
+        ' ... Auto-generated initialization code ...
+    End Sub
+End Class
